2eb69f4cf2b49053b9324b147687426bb11763ef
lorenzo.poluzzi <lorenzo.poluzzi@netserv.it>
2020-12-04 17:25:53 +0100

    Init project

---
+++ src/bollettinopostale/BollettinoPostale.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bollettinopostale;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.fop.apps.FOPException;
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.MimeConstants;
+
+/**
+ *
+ * @author lorenzo.poluzzi
+ */
+public class BollettinoPostale {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BollettinoPostale bollettinoPostale = new BollettinoPostale();
+        try {
+            bollettinoPostale.convertToPDF();
+        } catch (FOPException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Method that will convert the given XML to PDF 
+     * @throws IOException
+     * @throws FOPException
+     * @throws TransformerException
+     */
+    public void convertToPDF()  throws IOException, FOPException, TransformerException {
+        // the XSL FO file
+        File xsltFile = new File("bollettino.xsl");
+        // the XML file which provides the input
+        StreamSource xmlSource = new StreamSource(new File("bollettino.xml"));
+        // create an instance of fop factory
+        FopFactory fopFactory = FopFactory.newInstance();
+        // a user agent is needed for transformation
+        FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
+        // Setup output
+        OutputStream out;
+        out = new java.io.FileOutputStream("bollettino.pdf");
+    
+        try {
+            // Construct fop with desired output format
+            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
+
+            // Setup XSLT
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer(new StreamSource(xsltFile));
+
+            // Resulting SAX events (the generated FO) must be piped through to FOP
+            Result res = new SAXResult(fop.getDefaultHandler());
+
+            // Start XSLT transformation and FOP processing
+            // That's where the XML is first transformed to XSL-FO and then 
+            // PDF is created
+            transformer.transform(xmlSource, res);
+        } finally {
+            out.close();
+        }
+    }
+    
+}
